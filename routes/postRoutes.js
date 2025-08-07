fe6b3a1b57be985a91e08e4d6dbde654969df315
2459005-dot <2459005@kbu.ac.kr>
2025-08-07 11:40:53 +0900

    .

---
+++ routes/postRoutes.js
@@ -0,0 +1,10 @@
+// routes/character.js
+
+const express = require("express");
+const Post = require("../models/postModel"); 
+// 파일명과 맞춤
+const router = express.Router();
+
+
+
+module.exports = router;
