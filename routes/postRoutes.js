c188171c79ce92da1173f7c6fb0eb2ef3b075e08
2459005-dot <2459005@kbu.ac.kr>
2025-08-07 17:25:05 +0900

    .

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -26,6 +26,73 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.status(200).json({ message: '전체 게시글 조회', posts })
+    } catch (error) {
+        res.status(500).json({ message: "서버 오류" })
+    }
+})
 
+router.get('/:id', async (req, res) => {
+    try {
+        const postId = req.params.id
+
+        const post = await Post.findById(postId)
+
+        if (!post) {
+            res.status(404).json({ message: '게시글 없음' })
+        }
+
+        res.status(200).json({ message: '게시글 조회', post })
+    } catch (error) {
+        res.status(500).json({ message: "서버 오류" })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { title, content, author, isPublished, tags } = req.body
+        if (!title || !content) {
+            return res.status(400).json({ message: '제목과 내용은 필수입력' })
+        }
+        const updatePost = await Post.findByIdAndUpdate(
+            req.params.id,
+            {
+                title,
+                content,
+                author,
+                isPublished,
+                tags
+            },
+            {
+                new: true,
+                runValidators: true
+            })
+        if (!updatePost) {
+            res.status(404).json({ message: '게시글 없음' })
+        }
+        res.status(200).json({ message: '캐릭터 수정', post: updatePost })
+    } catch (error) {
+        res.status(500).json({ message: '서버 오류', error })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const postId = req.params.id
+
+        const post = await Post.findByIdAndDelete(postId)
+
+        if (!post) {
+            res.status(404).json({ message: '게시글 없음' })
+        }
+
+        res.status(200).json({ message: '게시글 삭제', post })
+    } catch (error) {
+        res.status(500).json({ message: '서버 오류', error })
+    }
+})
 
 module.exports = router;
