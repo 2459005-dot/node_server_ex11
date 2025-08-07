424e5444e5417c0bfaea13fa7ce4baa77254baa7
2459005-dot <2459005@kbu.ac.kr>
2025-08-07 12:11:51 +0900

    .

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,17 +1,31 @@
 // routes/character.js
 
 const express = require("express");
-const Post = require("../models/postModel"); 
+const Post = require("../models/postModel");
 // 파일명과 맞춤
 const router = express.Router();
 
-router.post('/', async(req,res)=>{
+router.post('/', async (req, res) => {
     try {
-        const {title, content, author, isPublished, tags} = req.body
-        
+        const { title, content, author, isPublished, tags } = req.body
+        if (!title || !content) {
+            return res.status(400).json({ message: '제목은 필수입니다.' })
+        }
+        const newPost = new Post({
+            title,
+            content,
+            author,
+            isPublished: isPublished ?? false,
+            tags
+        })
+        const savePost = await newPost.save()
+
+        res.status(200).json({ message: '게시글 추가', post: savePost })
     } catch (error) {
-        
+        res.status(500).json({ message: '서버 오류', error })
     }
 })
+
+
 
 module.exports = router;
